3db814884b777244fc7bda1763181ea1c46b8aed
John Nguyen <jnguyen92780@gmail.com>
2020-07-10 13:25:49 -0700

    WIP

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+// $.ajax({
+//   method: "GET",
+//   url: "https://jsonplaceholder.typicode.com/users",
+//   success: generateUserData,
+//   error: function(err) {
+//     console.log(err)
+//   }
+// })
+
+// function generateUserData(data) {
+//   const userData = document.getElementById("user-data");
+//   for (let i = 0; i < data.length; i++) {
+//     const tr = document.createElement("tr");
+//     const userId = document.createElement("td");
+//     const name = document.createElement("td");
+//     const email = document.createElement("td");
+//     userId.textContent = data[i].id;
+//     name.textContent = data[i].name;
+//     email.textContent = data[i].email;
+//     tr.append(userId);
+//     tr.append(name);
+//     tr.append(email);
+//     userData.append(tr);
+//   }
+// }
+
 let googleDataToSend = {
   "requests": [
     {
