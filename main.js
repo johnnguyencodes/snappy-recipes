80b3e86bd5cbaae796e2992006f03d353e12d4b7
John Nguyen <jnguyen92780@gmail.com>
2020-07-24 12:18:38 -0700

    added upload percentage bar for imgur API upload

--- main.js
+++ main.js
@@ -110,7 +110,33 @@ function startImgurAPI(formData) {
   headers: {
     "Authorization": "Client-ID 62cbd49ff79d018"
     },
+      xhr: function () {
+        var xhr = new window.XMLHttpRequest();
+        xhr.upload.addEventListener("progress", function (evt) {
+          if (evt.lengthComputable) {
+            var percentComplete = evt.loaded / evt.total;
+            console.log(percentComplete);
+            $('.progress').css({
+              width: percentComplete * 100 + '%'
+            });
+            if (percentComplete === 1) {
+              $('.progress').addClass('hide');
+            }
+          }
+        }, false);
+        xhr.addEventListener("progress", function (evt) {
+          if (evt.lengthComputable) {
+            var percentComplete = evt.loaded / evt.total;
+            console.log(percentComplete);
+            $('.progress').css({
+              width: percentComplete * 100 + '%'
+            });
+          }
+        }, false);
+        return xhr;
+      },
   success: function(data) {
+    $("#status").html('UPLOADED!!');
     const imageURL = data.data.link;
     googleDataToSend.requests[0].image.source.imageUri = imageURL;
     imageOnPage(imageURL);
