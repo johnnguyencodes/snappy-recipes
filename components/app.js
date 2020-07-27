ac69629fa2e61b6575c52f111c9264469f00105c
John Nguyen <jnguyen92780@gmail.com>
2020-07-26 18:37:17 -0700

    app.js refactored

--- components/app.js
+++ components/app.js
@@ -1,3 +1,26 @@
+let dataForImageRecognition = {
+  "requests": [
+    {
+      "image": {
+        "source": {
+          "imageUri": null
+        }
+      },
+      "features": [
+        {
+          "type": "LABEL_DETECTION"
+        }
+      ]
+    }
+  ]
+};
+
+
+let spoonacularDataToSend = {
+  "diet": null,
+  "intolerances": null
+}
+
 class App {
   constructor(pageHeader, dietForm, imageTitleContainer, recipesContainer) {
     this.pageHeader = pageHeader,
@@ -6,10 +29,14 @@ class App {
     this.recipesContainer = recipesContainer;
     this.imgValidation = this.imgValidation.bind(this);
     this.postImage = this.postImage.bind(this);
-    this.imageRecognition = this.imageRecognition.bind(this);
-    this.getRecipes = this.getRecipes.bind(this);
     this.handlePostImageSuccess = this.handlePostImageSuccess.bind(this);
     this.handlePostImageError = this.handlePostImageError.bind(this);
+    this.imageRecognition = this.imageRecognition.bind(this);
+    this.handleImageRecognitionSuccess = this.handleImageRecognitionSuccess.bind(this);
+    this.handleImageRecognitionError = this.handleImageRecognitionError.bind(this);
+    this.getRecipes = this.getRecipes.bind(this);
+    this.handleGetRecipesSuccess = this.handleGetRecipesSuccess.bind(this);
+    this.handleGetRecipesError = this.handleGetRecipesError.bind(this);
   }
 
   start() {
@@ -51,121 +78,106 @@ class App {
     fileInput.value = "";
   }
 
-//*App Begin
+  //*App Begin
 
-//POST request to IMGUR with image id supplied
-postImage(formData) {
-  $.ajax({
-    method: "POST",
-    url: "https://api.imgur.com/3/image/",
-    data: formData,
-    processData: false,
-    contentType: false,
-    cache: false,
-    headers: {
-      "Authorization": "Client-ID 62cbd49ff79d018"
-    },
-    xhr: function () {
-      var xhr = new window.XMLHttpRequest();
-      xhr.upload.addEventListener("progress", function (evt) {
-        if (evt.lengthComputable) {
-          var percentComplete = evt.loaded / evt.total;
-          $('#upload_progress').css({
-            width: percentComplete * 100 + '%'
-          });
-          if (percentComplete > 0 && percentComplete < 1) {
-            $('#image_upload_container').removeClass('d-none');
-          }
-          if (percentComplete === 1) {
-            $('#image_upload_container').addClass('d-none');
-          }
-        }
-      }, false);
-      return xhr;
-    },
-    success: this.handlePostImageSuccess,
-    error: this.handlePostImageError
-  })
-}
-
-handlePostImageSuccess(data) {
-  const imageURL = data.data.link;
-  dataForImageRecognition.requests[0].image.source.imageUri = imageURL;
-  imageOnPage(imageURL);
-  imageRecognition();
-}
-
-handlePostImageError(error) {
-  console.error(error)
-}
-
-
-let dataForImageRecognition = {
-  "requests": [
-    {
-      "image": {
-        "source": {
-          "imageUri": null
-        }
+  //POST request to IMGUR with image id supplied
+  postImage(formData) {
+    $.ajax({
+      method: "POST",
+      url: "https://api.imgur.com/3/image/",
+      data: formData,
+      processData: false,
+      contentType: false,
+      cache: false,
+      headers: {
+        "Authorization": "Client-ID 62cbd49ff79d018"
       },
-      "features": [
-        {
-          "type": "LABEL_DETECTION"
-        }
-      ]
+      xhr: function () {
+        var xhr = new window.XMLHttpRequest();
+        xhr.upload.addEventListener("progress", function (evt) {
+          if (evt.lengthComputable) {
+            var percentComplete = evt.loaded / evt.total;
+            $('#upload_progress').css({
+              width: percentComplete * 100 + '%'
+            });
+            if (percentComplete > 0 && percentComplete < 1) {
+              $('#image_upload_container').removeClass('d-none');
+            }
+            if (percentComplete === 1) {
+              $('#image_upload_container').addClass('d-none');
+            }
+          }
+        }, false);
+        return xhr;
+      },
+      success: this.handlePostImageSuccess,
+      error: this.handlePostImageError
+    })
     }
-  ]
-};
 
-//POST request to Google's Cloud Vision API with image from IMGUR to label the object in the image
-imageRecognition() {
-  document.getElementById("title_download_text").className = "text-center";
-  $.ajax({
-    url: "https://vision.googleapis.com/v1/images:annotate?fields=responses&key=AIzaSyAJzv7ThEspgv8_BxX2EwCs8PUEJMtJN6c",
-    type: "POST",
-    dataType: "JSON",
-    contentType: "application/json",
-    data: JSON.stringify(dataForImageRecognition),
-    success: function (response) {
-      if (!(response.responses[0].labelAnnotations)) {
-        alert("Sorry, your image could not be recognized. Please upload a different image or enter a search");
-        document.getElementById("my_image").src = "";
-        return;
-      }
-      const imageTitle = response.responses[0].labelAnnotations[0].description;
-      imageTitleOnPage(imageTitle);
-      document.getElementById("title_download_text").className = "text-center d-none";
-      getRecipes(imageTitle);
-    },
-    error: function (err) {
-      console.log(err);
+  handlePostImageSuccess(data) {
+    const imageURL = data.data.link;
+    dataForImageRecognition.requests[0].image.source.imageUri = imageURL;
+    imageOnPage(imageURL);
+    imageRecognition();
+  }
+
+  handlePostImageError(error) {
+    console.error(error)
+  }
+
+  //POST request to Google's Cloud Vision API with image from IMGUR to label the object in the image
+  imageRecognition() {
+    document.getElementById("title_download_text").className = "text-center";
+    $.ajax({
+      url: "https://vision.googleapis.com/v1/images:annotate?fields=responses&key=AIzaSyAJzv7ThEspgv8_BxX2EwCs8PUEJMtJN6c",
+      type: "POST",
+      dataType: "JSON",
+      contentType: "application/json",
+      data: JSON.stringify(dataForImageRecognition),
+      success: this.handleImageRecognitionSuccess;
+      error: this.handleImageRecognitionError;
+    });
+  }
+
+  handleImageRecognitionSuccess(response) {
+    if (!(response.responses[0].labelAnnotations)) {
+      alert("Sorry, your image could not be recognized. Please upload a different image or enter a search");
+      document.getElementById("my_image").src = "";
+      return;
     }
-  });
-}
+    const imageTitle = response.responses[0].labelAnnotations[0].description;
+    imageTitleOnPage(imageTitle);
+    document.getElementById("title_download_text").className = "text-center d-none";
+    getRecipes(imageTitle);
+  }
 
-let spoonacularDataToSend = {
-  "diet": null,
-  "intolerances": null
-}
+  handleImageRecognitionError(error) {
+    console.error(error);
+  }
 
-//GET request to Spoonacular's API with label from Google to get a list of up to 10 recipes containing the item from the image and other nutrition info.
-getRecipes(imageTitle) {
-  document.getElementById("recipe_download_text").className = "text-center";
-  var spoonacularURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + imageTitle + "&apiKey=5d83fe3f2cf14616a6ea74137c2be564&addRecipeNutrition=true"
-  $.ajax({
-    method: "GET",
-    url: spoonacularURL,
-    data: spoonacularDataToSend,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    success: function (data) {
-      recipeOnPage(data);
-    },
-    error: function (err) {
-      console.log(err);
-    }
-  })
-}
+  //GET request to Spoonacular's API with label from Google to get a list of up to 10 recipes containing the item from the image and other nutrition info.
+  getRecipes(imageTitle) {
+    document.getElementById("recipe_download_text").className = "text-center";
+    var spoonacularURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + imageTitle + "&apiKey=5d83fe3f2cf14616a6ea74137c2be564&addRecipeNutrition=true"
+    $.ajax({
+      method: "GET",
+      url: spoonacularURL,
+      data: spoonacularDataToSend,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      success: this.handleGetRecipesSuccess,
+      error: this.handleGetRecipesError
+    })
+  }
+
+  handleGetRecipesSuccess(recipes) {
+    recipeOnPage(recipes);
+  }
+
+  handleGetRecipesError(error) {
+    console.error(error);
+  }
 
 }
