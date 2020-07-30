89ce2bc6223046df3f8d3597034d252186836cd4
John Nguyen <jnguyen92780@gmail.com>
2020-07-30 15:59:12 -0700

    reset function complete

--- components/app.js
+++ components/app.js
@@ -4,10 +4,8 @@ const spoonacularAPIKey = config.spoonacularAPIKey;
 const dietMenu = document.getElementById("diet_menu");
 let fileInputForm = document.getElementById("file_input_form");
 const fileLabel = document.getElementById("custom_file_label");
-const title = document.getElementById("title");
 const searchInput = document.getElementById("recipe_search_input");
 const image = document.getElementById("my_image");
-const recipe = document.getElementById("recipe");
 const inputs = document.querySelectorAll(".input");
 const recipeDownloadText = document.getElementById("recipe_download_text");
 const uploadButton = document.getElementById("upload_button");
@@ -75,7 +73,6 @@ class App {
 
 
   dietInfo() {
-    console.log("hello");
     let restrictionValues = "";
     let intoleranceValues = "";
     var restrictionCheckboxes = document.getElementsByClassName("restrictionCheckbox");
