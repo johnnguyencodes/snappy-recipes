7d78d29c44b4ea66aca8e53764dfe2fa2af81ba6
John Nguyen <jnguyen92780@gmail.com>
2020-08-11 12:13:03 -0700

    working on favorite menu closing and opening behavior

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {
   imgurAPIKey: 'Client-ID 62cbd49ff79d018',
   googleAPIKey: 'AIzaSyAJzv7ThEspgv8_BxX2EwCs8PUEJMtJN6c',
-  spoonacularAPIKey: '7a1c7fed3aea4c248ece4191a7bc43c1'
+  spoonacularAPIKey: 'f4e6431e5f814317bb427f0aa522df24'
 }
 
 // f4e6431e5f814317bb427f0aa522df24
