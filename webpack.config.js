14796439b6daaece8e02dbe7199df17a1cb7bcf9
John Nguyen <jnguyen92780@gmail.com>
2024-09-19 19:47:14 -0700

    prep to build app using webpack during vercel deployment

--- webpack.config.js
+++ webpack.config.js
@@ -11,28 +11,6 @@ module.exports = {
   },
   plugins: [
     new Dotenv(),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(
-        process.env.NODE_ENV || "development"
-      ),
-      "process.env.IMGUR_CLIENT_ID": JSON.stringify(
-        process.env.IMGUR_CLIENT_ID
-      ),
-      "process.env.IMGUR_CLIENT_SECRET": JSON.stringify(
-        process.env.IMGUR_CLIENT_SECRET
-      ),
-      "process.env.IMGUR_ALBUM_ID": JSON.stringify(process.env.IMGUR_ALBUM_ID),
-      "process.env.IMGUR_AUTHORIZATION_CODE": JSON.stringify(
-        process.env.IMGUR_AUTHORIZATION_CODE
-      ),
-      "process.env.IMGUR_REFRESH_TOKEN": JSON.stringify(
-        process.env.IMGUR_REFRESH_TOKEN
-      ),
-      "process.env.SPOONACULAR_API_KEY": JSON.stringify(
-        process.env.SPOONACULAR_API_KEY
-      ),
-      "process.env.GOOGLE_API_KEY": JSON.stringify(process.env.GOOGLE_API_KEY),
-    }),
     new CopyWebpackPlugin({
       patterns: [
         { from: "./index.html", to: "./" }, // Copy index.html to dist folder
