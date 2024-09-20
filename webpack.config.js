6cb4a62be2e907a27dc5bc3808ec8f6d03ef7dca
John Nguyen <jnguyen92780@gmail.com>
2024-09-19 20:19:06 -0700

    changed build folder to public

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/main.js", // Adjust this to your entry file path
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
   },
   module: {
     rules: [
